221dbb04b87f1291de0d324f035ffdaf7d7327fb
zhouzhanzhao123 <2115516135@qq.com>
2020-07-11 17:02:59 +0800

    1、utils目录移动，加了评估函数2、输出结果到日志

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -10,3 +10,7 @@ python ./tensorflow/run.py --prepare \
                     --max_a_len 80 \
                   	--train_files /match_bidaf/train_para_extra.json \
                   	--test_files /match_bidaf/test.json \
+                  	--dev_files /match_bidaf/test.json
+
+
+#python ./tensorflow/run.py --prepare --train --predict --algo BIDAF --epoch 10 --max_q_len 80 --max_a_len 80 --train_files ./data/du_format/train_para_extra_small.json --test_files ./data/du_format/test_small.json --dev_files ./data/du_format/test_small.json
