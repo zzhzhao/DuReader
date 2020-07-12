fa337411ae03107bc0f95095661d97583f226c7a
zhouzhanzhao123 <2115516135@qq.com>
2020-07-12 16:45:38 +0800

    把paragraph_extraction中含答案的句子重新放到训练集的最前面

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -7,9 +7,9 @@ python ./tensorflow/run.py --prepare \
                     --epoch 10 \
                     --max_q_len 80 \
                     --max_a_len 80 \
-                  	--train_files /match_bidaf_data/train_para_extra.json \
-                  	--test_files /match_bidaf_data/test_para_extra.json \
-                  	--dev_files /match_bidaf_data/test_para_extra.json
+                  	--train_files /match_lstm_bidaf_data/train_para_extra.json \
+                  	--test_files /match_lstm_bidaf_data/test_para_extra.json \
+                  	--dev_files /match_lstm_bidaf_data/test_para_extra.json
 
 
 #python ./tensorflow/run.py --prepare --train --predict --algo BIDAF --epoch 10 --max_q_len 80 --max_a_len 80 --train_files ./data/du_format/train_para_extra_small.json --test_files ./data/du_format/test_small.json --dev_files ./data/du_format/test_small.json
